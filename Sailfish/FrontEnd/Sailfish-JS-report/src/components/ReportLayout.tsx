78e188eb9fe4fe6f52f19ddde40514f09b89461a
vladislav.kotelnikov <vladislav.kotelnikov@exactprosystems.com>
2019-01-31 16:53:36 +0300

    Feature #52061. Add license header to all files.
    (cherry picked from commit 76a871b719cf6b67a090fbd16a8ebd3fbddcb016)
    
    Change-Id: I77ab82aaa3db59b8f9eb30e671ab365d1af2184b

--- Sailfish/FrontEnd/Sailfish-JS-report/src/components/ReportLayout.tsx
+++ Sailfish/FrontEnd/Sailfish-JS-report/src/components/ReportLayout.tsx
@@ -1,3 +1,19 @@
+/******************************************************************************
+* Copyright 2009-2019 Exactpro (Exactpro Systems Limited)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+******************************************************************************/
+
 import { h } from 'preact';
 import Report from '../models/Report';
 import TestCase from '../models/TestCase';
@@ -15,6 +31,10 @@ interface ReportLayoutProps {
 const ReportLayoutBase = ({ report, onTestCaseSelect }: ReportLayoutProps) => {
 
     const executionTime = getSecondsPeriod(report.startTime, report.finishTime);
+        
+    // use *1000 to get correct time
+    const date = new Date(report.startTime * 1000).toString();
+    
     let testCaseCount = 1;
 
     return (
@@ -49,7 +69,7 @@ const ReportLayoutBase = ({ report, onTestCaseSelect }: ReportLayoutProps) => {
                             </tr>
                             <tr>
                                 <td class="report-info-table-name">Date:</td>
-                                <td>{report.startTime}</td>
+                                <td>{date}</td>
                             </tr>
                             <tr>
                                 <td class="report-info-table-name">Version:</td>
