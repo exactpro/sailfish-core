2a4f49928bfb3e8b9c19483ca8771cef8a82588a
oleg.smirnov <oleg.smirnov@exactprosystems.com>
2020-04-29 13:48:50 +0300

    [RM] Bug #59214. Move test to the TesFIXHumanMessage class
    Change-Id: Iedf59e7ea1dd4ff8981c4b9102d46d3a2861343a

--- BackEnd/Service/service-fix/src/test/java/com/exactpro/sf/services/fix/converter/QFJMessage2IMessageTest.java
+++ BackEnd/Service/service-fix/src/test/java/com/exactpro/sf/services/fix/converter/QFJMessage2IMessageTest.java
@@ -24,7 +24,6 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.quickfixj.CharsetSupport;
 
 import com.exactpro.sf.common.messages.IMessage;
@@ -34,13 +33,10 @@ import com.exactpro.sf.comparison.ComparatorSettings;
 import com.exactpro.sf.comparison.ComparisonResult;
 import com.exactpro.sf.comparison.ComparisonUtil;
 import com.exactpro.sf.comparison.MessageComparator;
-import com.exactpro.sf.configuration.IDictionaryManager;
 import com.exactpro.sf.configuration.factory.FixMessageFactory;
 import com.exactpro.sf.scriptrunner.StatusType;
 import com.exactpro.sf.services.fix.FixUtil;
 import com.exactpro.sf.services.fix.QFJDictionaryAdapter;
-import com.exactpro.sf.storage.util.JsonIMessageDecoder;
-import com.exactpro.sf.storage.util.JsonMessageDecoder;
 import com.exactpro.sf.util.ConverterTest;
 import com.exactpro.sf.util.DateTimeUtility;
 
@@ -122,86 +118,6 @@ public class QFJMessage2IMessageTest extends ConverterTest {
         System.out.println(MessageUtil.convertToIHumanMessage(messageFactory, dictionary.getMessages().get(iMessage.getName()), iMessage));
 
         //quickfix.Message fixMessageTarget = converter.convert(iMessage, true);
-
-    }
-
-    @Test
-    public void testDirtyFIXHumanFormat() throws Exception {
-
-        String msg = "{\n"
-                + "    \"id\": 2165752,\n"
-                + "    \"timestamp\": 1570183516009,\n"
-                + "    \"name\": \"NewOrderSingle\",\n"
-                + "    \"namespace\": \"FIX_5_0\",\n"
-                + "    \"dictionaryURI\": \"FIX_5_0\",\n"
-                + "    \"protocol\": \"FIX\",\n"
-                + "    \"rejectReason\": null,\n"
-                + "    \"admin\": false,\n"
-                + "    \"dirty\": true,\n"
-                + "    \"message\": {\n"
-                + "        \"AccountType\": \"3\",\n"
-                + "        \"ClOrdID\": \"excluded field\",\n"
-                + "        \"DisplayQty\": \"769\",\n"
-                + "        \"OrdType\": \"1\",\n"
-                + "        \"OrderCapacity\": \"R\",\n"
-                + "        \"OrderQty\": \"896\",\n"
-                + "        \"SecurityID\": \"8011001\",\n"
-                + "        \"SecurityIDSource\": \"8\",\n"
-                + "        \"Side\": \"2\",\n"
-                + "        \"TradingParty\": {\n"
-                + "            \"NoPartyIDs\": [\n"
-                + "                {\n"
-                + "                    \"PartyID\": \"NGALL1FX01\",\n"
-                + "                    \"PartyIDSource\": \"D\",\n"
-                + "                    \"PartyRole\": \"76\"\n"
-                + "                },\n"
-                + "                {\n"
-                + "                    \"PartyID\": \"0\",\n"
-                + "                    \"PartyIDSource\": \"P\",\n"
-                + "                    \"PartyRole\": \"3\"\n"
-                + "                },\n"
-                + "                {\n"
-                + "                    \"PartyID\": \"3\",\n"
-                + "                    \"PartyIDSource\": \"P\",\n"
-                + "                    \"PartyRole\": \"12\"\n"
-                + "                },\n"
-                + "                {\n"
-                + "                    \"PartyID\": \"0\",\n"
-                + "                    \"PartyIDSource\": \"P\",\n"
-                + "                    \"PartyRole\": \"122\"\n"
-                + "                }\n"
-                + "            ]\n"
-                + "        },\n"
-                + "        \"TransactTime\": \"20180205-10:38:08.000008\",\n"
-                + "        \"header\": {\n"
-                + "            \"ApplVerID\": \"9\",\n"
-                + "            \"BeginString\": \"FIXT.1.1\",\n"
-                + "            \"BodyLength\": \"239\",\n"
-                + "            \"MsgSeqNum\": \"2\",\n"
-                + "            \"MsgType\": \"D\",\n"
-                + "            \"SenderCompID\": \"NGALL1FX01\",\n"
-                + "            \"SendingTime\": \"20191004-10:05:16.010000\",\n"
-                + "            \"TargetCompID\": \"FGW\"\n"
-                + "        },\n"
-                + "        \"trailer\": {\n"
-                + "            \"CheckSum\": \"055\"\n"
-                + "        }\n"
-                + "    }\n"
-                + "}";
-
-
-        IDictionaryStructure dictionary = getSfDictionary("FIX50.TEST.xml");
-
-        JsonMessageDecoder<IMessage> jsonMessageDecoder = new JsonIMessageDecoder(null);
-
-        IMessage iMessage = jsonMessageDecoder.decode(msg, true);
-
-        Assert.assertEquals("header={BeginString(8)=FIXT.1.1; BodyLength(9)=239; MsgType(35)=ORDER_SINGLE(D); SenderCompID(49)=NGALL1FX01; TargetCompID(56)=FGW; MsgSeqNum(34)=2; "
-                + "SendingTime(52)=20191004-10:05:16.010000; ApplVerID(1128)=FIX50SP2(9)}; ClOrdID(11)=excluded field; TradingParty={NoPartyIDs(453)=[{PartyID(448)=NGALL1FX01; "
-                + "PartyIDSource(447)=PROPRIETARY_CUSTOM_CODE(D); PartyRole(452)=DESK_ID(76)}; {PartyID(448)=0; PartyIDSource(447)=P; PartyRole(452)=CLIENT_ID(3)}; {PartyID(448)=3; PartyIDSource(447)=P; PartyRole(452)=EXECUTING_TRADER(12)}; {PartyID(448)=0; "
-                + "PartyIDSource(447)=P; PartyRole(452)=122}]}; SecurityID(48)=8011001; SecurityIDSource(22)=EXCHANGE_SYMBOL(8); OrdType(40)=MARKET(1); Side(54)=SELL(2); OrderQty(38)=896; DisplayQty(1138)=769; AccountType(581)=HOUSE(3); OrderCapacity(528)=RISKLESS_PRINCIPAL(R); "
-                + "TransactTime(60)=20180205-10:38:08.000008; trailer={CheckSum(10)=055}", MessageUtil.convertToIHumanMessage(messageFactory, dictionary.getMessages().get(iMessage.getName()), iMessage).toString());
-
 
     }
 
