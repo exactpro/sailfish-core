467d67a2b0ef66a740e0038200eab4ccb77cc520
oleg.smirnov <oleg.smirnov@exactprosystems.com>
2020-10-10 19:51:44 +0300

    TH2 #726. Handle EvolutionBatch message when service requests human readable format for message
    Change-Id: I13d1e170a9f022ee4af0fa9be2f99b25fdf46b5f

--- BackEnd/Core/sailfish-core/src/main/java/com/exactpro/sf/services/AbstractInitiatorService.java
+++ BackEnd/Core/sailfish-core/src/main/java/com/exactpro/sf/services/AbstractInitiatorService.java
@@ -69,6 +69,9 @@ public abstract class AbstractInitiatorService extends AbstractService implement
     }
 
     protected String getHumanReadable(IMessage message) {
+        if (EvolutionBatch.MESSAGE_NAME.equals(message.getName()) && getSettings().isEvolutionSupportEnabled()) {
+            return message.toString();
+        }
         IMessageStructure messageStructure = dictionary.getMessages().get(message.getName());
 
         if(messageStructure == null) {
