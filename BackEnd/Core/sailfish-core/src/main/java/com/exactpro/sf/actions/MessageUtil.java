f0bccf489de0f84f0055977dae3010bd49fa9907
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2024-11-28 11:16:10 +0400

    [RMA] Feature #52315. Added Existence method to the MessageUtil
    Change-Id: If531f760aacc19ec03420c41aa3eb3279d378507

--- BackEnd/Core/sailfish-core/src/main/java/com/exactpro/sf/actions/MessageUtil.java
+++ BackEnd/Core/sailfish-core/src/main/java/com/exactpro/sf/actions/MessageUtil.java
@@ -1,5 +1,5 @@
-/******************************************************************************
- * Copyright 2009-2018 Exactpro (Exactpro Systems Limited)
+/*
+ * Copyright 2009-2024 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 package com.exactpro.sf.actions;
 
 import java.util.List;
@@ -85,6 +85,14 @@ public class MessageUtil extends AbstractCaller {
     @UtilityMethod
     public Object Empty() {
         return Convention.CONV_MISSED_OBJECT;
+    }
+
+    @Description("Returns the special object representing an existence value.<br/>" +
+            "Example:<br/>" +
+            "#{Existence()} returns \"EXISTENCE\"")
+    @UtilityMethod
+    public Object Existence() {
+        return Convention.CONV_EXISTENCE_OBJECT;
     }
 
     @Description("Returns the regex filter object for specified pattern.<br/>" +
